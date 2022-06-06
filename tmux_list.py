5b776c29fa07ad7b5affe82ce61753321e8e9030
KeyesHsu <keyes.hsu@gmail.com>
2022-06-06 16:15:27 +0800

    Ignore case in search patterns

--- tmux_list.py
+++ tmux_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from __future__ import annotations
-
 import re
 import json
 import libtmux
@@ -10,12 +8,13 @@ import sys
 from typing import Dict, List, Union, Optional
 
 
-
 class TmuxList:
     def __init__(self):
         self.server = libtmux.Server()
 
-    def list_sessions(self, filter_name: Optional[str]=None) -> Dict[str, Union[str, List, Dict]]:
+    def list_sessions(
+        self, filter_name: Optional[str] = None
+    ) -> List[Dict[str, Union[str, List, Dict]]]:
         result = []
 
         sessions = self.server.list_sessions()
@@ -23,23 +22,25 @@ class TmuxList:
             session_id = session.get("session_id")
             session_name = session.get("session_name")
 
-            if filter_name and not re.match(r".*%s.*" % filter_name, session_name):
+            if filter_name and not re.match(
+                r".*%s.*" % filter_name.lower(), session_name.lower()
+            ):
                 continue
 
             result.append(
-                    {
-                        "title": session_name,
-                        "subtitle": f"Session {session_name}",
-                        "arg": ["session",session_id, session_name],
-                        "autocomplete": "",
-                        "icon": {
-                            "path": "icons/session.ico"
-                            }
-                        }
-                    )
+                {
+                    "title": session_name,
+                    "subtitle": f"Session {session_name}",
+                    "arg": ["session", session_id, session_name],
+                    "autocomplete": "",
+                    "icon": {"path": "icons/session.ico"},
+                }
+            )
         return result
 
-    def list_windows(self, filter_name: Optional[str]=None) -> Dict[str, Union[str, List, Dict]]:
+    def list_windows(
+        self, filter_name: Optional[str] = None
+    ) -> List[Dict[str, Union[str, List, Dict]]]:
         result = []
 
         sessions = self.server.list_sessions()
@@ -52,30 +53,28 @@ class TmuxList:
                 window_id = window.get("window_id")
                 window_index = window.get("window_index")
 
-                if filter_name and not re.match(r".*%s.*" % filter_name, window_name):
+                if filter_name and not re.match(
+                    r".*%s.*" % filter_name.lower(), window_name.lower()
+                ):
                     continue
 
                 result.append(
-                        {
-                            "title": window_name,
-                            "subtitle": f"Window {window_index}:{window_name} in session {session_name}",
-                            "arg": ["window", f"{session_id}-{window_id}", window_name],
-                            "autocomplete": "",
-                            "icon": {
-                                "path": "icons/window.ico"
-                                }
-                            }
-                        )
+                    {
+                        "title": window_name,
+                        "subtitle": f"Window {window_index}:{window_name} in session {session_name}",
+                        "arg": ["window", f"{session_id}-{window_id}", window_name],
+                        "autocomplete": "",
+                        "icon": {"path": "icons/window.ico"},
+                    }
+                )
         return result
-
 
     def print(self, *args):
         items = []
         for arg in args:
             items.extend(arg)
 
-        print(json.dumps({ "items": items }))
-
+        print(json.dumps({"items": items}))
 
 
 if __name__ == "__main__":
@@ -105,4 +104,3 @@ if __name__ == "__main__":
             list_session_result = tmux_list.list_sessions(filter_name)
             list_window_result = tmux_list.list_windows(filter_name)
             tmux_list.print(list_session_result, list_window_result)
-
